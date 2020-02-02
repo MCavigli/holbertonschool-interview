a52907ada3320cc42d8b33b4be941be831f26662
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-02-02 15:16:17 -0800

    Initial push for Task 0

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+""" Module that checks to see if all boxes are unlockable """
+
+# Box = __import__('box_class').Box
+
+def canUnlockAll(boxes):
+    """ Checks if all boxes are unlockable
+        Args: boxes - a list of lists
+        Return: True or False
+    """
+    if not isinstance(boxes, list):
+        return False
+    if len(boxes) == 1:
+        return True
+    if len(boxes) == 0 or len(boxes[0]) == 0:
+        return False
+    boxDict = dictInit(boxes)
+    boxOfKeys = []
+    for key in boxes[0]:
+        boxOfKeys.append(key)
+    while ('locked' in boxDict.values() and len(boxOfKeys) > 0):
+        boxOfKeys = unlockBoxes(boxes, boxDict, boxOfKeys)
+    if ('locked' in boxDict.values()):
+        return False
+    else:
+        return True
+
+def unlockBoxes(boxes, boxDict, boxOfKeys):
+    newKeys = []
+    for key in boxOfKeys:
+        if boxDict[key] == 'locked':
+            boxDict[key] = 'unlocked'
+            if len(boxes[key]) == 0:
+                continue
+            else:
+                for nk in boxes[key]:
+                    newKeys.append(nk)
+    boxOfKeys.clear()
+    for key in newKeys:
+        boxOfKeys.append(key)
+    list(set(boxOfKeys))
+    newKeys.clear()
+    return boxOfKeys
+
+
+
+    
+
+
+
+
+def dictInit(box):
+    """ Initializes the dictionary """
+    boxDict = {}
+    for i in range(len(box)):
+        boxDict[i] = 'locked'
+    boxDict[0] = 'unlocked'
+
+    return boxDict
+
+def dictCheck(boxDict):
+    """ Checks if all values are unlocked """
+    if 'locked' in boxDict.values():
+        return False
+    return True
