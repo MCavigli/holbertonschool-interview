2275d85efa5ae6cca1e8d58d82130ea9e8f7dc88
Marc Cavigli <mcavigli@Marcs-MacBook-Pro.local>
2020-02-09 17:47:44 -0800

    Added helper function to put in new node if the list is only one node long

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -14,40 +14,63 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	if (!head)
 		return (NULL);
-	listint_t *new;
 	listint_t *tmp = *head;
-	listint_t *slow = *head;
+	listint_t *new;
 
-	new = malloc(sizeof(listint_t));
 	if (!tmp->next)
 	{
-		if (slow->n < number)
-		{
-			new = add_nodeint_end(head, number);
-			return (&new);
-		}
-		else
-		{
-			new->next = slow;
-			new->n = number;
-			*head = new;
-			return (&new);
-		}
+		new = single_node_list(head, number);
+		return (new);
 	}
-	tmp = tmp->next;
+	new = malloc(sizeof(listint_t));
+	new->n = number;
 
 	while (tmp)
 	{
-		if (number > slow->n && number < tmp->n)
+		if (!tmp->next && tmp->n < number)
 		{
-			new->n = number;
-			slow->next = new;
+			tmp->next = new;
+			new->next = NULL;
+			return (new);
+		}
+		if (number > tmp->n && number < tmp->next->n)
+		{
+			new->next = tmp->next;
+			tmp->next = new;
+			return (new);
+		}
+		if (number < tmp->n)
+		{
 			new->next = tmp;
-			return (&new);
+			*head = new;
+			return (new);
 		}
 		tmp = tmp->next;
-		slow = slow->next;
 	}
 	free_listint(new);
 	return (NULL);
+}
+
+/**
+ * single_node_list - Adds a new node if the list is only one node long
+ * @head: double pointer to first (and only) node in the list
+ * @number: the number to put in the new node
+ * Return: address of the new node
+ */
+listint_t *single_node_list(listint_t **head, int number)
+{
+	listint_t *tmp = *head;
+	listint_t *new = malloc(sizeof(listint_t));
+
+	if (tmp->n < number)
+	{
+		new = add_nodeint_end(head, number);
+	}
+	else
+	{
+		new->next = tmp;
+		new->n = number;
+		*head = new;
+	}
+	return (new);
 }
