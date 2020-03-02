d999b21b8db8c1c3e60ce2d5995665c12398eabb
Marc <mcavigli@gmail.com>
2020-03-02 13:44:42 -0800

    Setting up problem environment

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,11 @@
+#include "sandpiles.h"
+
+/**
+ * sandpiles_sum - computes the sum of two sandpiles
+ * @grid1: the first grid
+ * @grid2: the second grid
+ */
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+}
