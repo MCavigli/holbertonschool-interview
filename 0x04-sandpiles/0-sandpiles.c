09f6830a3fe099717602314b131348a810835c30
Marc <mcavigli@gmail.com>
2020-03-03 15:30:33 -0800

    Initial push

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -1,4 +1,6 @@
 #include "sandpiles.h"
+#include <stdbool.h>
+#include <stdio.h>
 
 /**
  * sandpiles_sum - computes the sum of two sandpiles
@@ -8,4 +10,77 @@
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
+	add_piles_together(grid1, grid2);
+	while (grid_unstable(grid1))
+	{
+		printf("=\n");
+		print_grid(grid1);
+		grid_dispersal(grid1, grid2);
+	}
+}
+
+/**
+ * add_piles_together - adds two sandpiles together
+ * @grid1: first grid
+ * @grid2: second grid
+ */
+
+void add_piles_together(int grid1[3][3], int grid2[3][3])
+{
+	for (int i = 0; i <= 2; i++)
+	{
+		for (int j = 0; j <= 2; j++)
+		{
+			grid1[i][j] += grid2[i][j];
+			grid2[i][j] = 0;
+		}
+	}
+}
+
+/**
+ * grid_unstable - checks if grid has unstable sandpiles
+ * @grid: grid to check
+ * Return: True if grid is unstable, false if fine
+ */
+
+bool grid_unstable(int grid[3][3])
+{
+	for (int i = 0; i <= 2; i++)
+	{
+		for (int j = 0; j <= 2; j++)
+		{
+			if (grid[i][j] > 3)
+				return (true);
+		}
+	}
+	return (false);
+}
+
+/**
+ * grid_dispersal - disperses sand in grid
+ * @grid1: grid to disperse
+ * @grid2: empty grid
+ */
+
+void grid_dispersal(int grid1[3][3], int grid2[3][3])
+{
+	for (int i = 0; i <= 2; i++)
+	{
+		for (int j = 0; j <= 2; j++)
+		{
+			if (grid1[i][j] > 3)
+			{
+				if (i > 0)
+					grid2[i - 1][j] += 1;
+				if (j > 0)
+					grid2[i][j - 1] += 1;
+				if (i < 2)
+					grid2[i + 1][j] += 1;
+				if (j < 2)
+					grid2[i][j + 1] += 1;
+				grid1[i][j] -= 4;
+			}
+		}
+	}
+	add_piles_together(grid1, grid2);
 }
--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,6 +1,10 @@
 #ifndef _SANDPILES_H_
 #define _SANDPILES_H_
-static void print_grid_sum(int grid1[3][3], int grid2[3][3]);
-static void print_grid(int grid[3][3]);
+#include <stdbool.h>
+void print_grid_sum(int grid1[3][3], int grid2[3][3]);
+void print_grid(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-#endif _SANDPILES_H_
+void add_piles_together(int grid1[3][3], int grid2[3][3]);
+bool grid_unstable(int grid[3][3]);
+void grid_dispersal(int grid1[3][3], int grid2[3][3]);
+#endif
