99ed120646c6e8446c9e6a79839f36a2dc151485
Marc <mcavigli@gmail.com>
2020-03-18 14:14:40 -0700

    Fixed pep issues

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,13 +5,8 @@ from signal import signal, SIGINT
 import sys
 
 
-def print_info(size, codes):
-    print("File size: {}".format(size))
-    for k, v in codes.items():
-        if v != 0:
-            print("{}: {}".format(k, v))
-
 if __name__ == '__main__':
+
     lineCount = 0
     totalSize = 0
     statusCodes = {"200": 0,
@@ -23,19 +18,25 @@ if __name__ == '__main__':
                    "405": 0,
                    "500": 0}
 
+    def print_info(size, codes):
+        print("File size: {}".format(size))
+        for k, v in codes.items():
+            if v != 0:
+                print("{}: {}".format(k, v))
+
     try:
         for line in sys.stdin:
             lineCount += 1
             info = line.split()
             totalSize += int(info[8])
-            
+
             if lineCount % 10 == 0:
                 print_info(totalSize, statusCodes)
             if info[7] in statusCodes:
                 statusCodes[info[7]] = statusCodes.get(info[7]) + 1
+            print_info(totalSize, statusCodes)
 
     except KeyboardInterrupt:
         print_info(totalSize, statusCodes)
-        #    raise
+        # raise
 
-    print_info(totalSize, statusCodes)
