bdede76de405e75ffc247726b21eb09ca6aaefdb
Marc <mcavigli@gmail.com>
2020-03-18 14:08:37 -0700

    Removed unnecessary signal line, moved final print_info() call, wrapped everything in a 'main' function, commented out KeyboardInterrupt raise call.

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -11,29 +11,31 @@ def print_info(size, codes):
         if v != 0:
             print("{}: {}".format(k, v))
 
-lineCount = 0
-totalSize = 0
-statusCodes = {"200": 0,
-               "301": 0,
-               "400": 0,
-               "401": 0,
-               "403": 0,
-               "404": 0,
-               "405": 0,
-               "500": 0}
+if __name__ == '__main__':
+    lineCount = 0
+    totalSize = 0
+    statusCodes = {"200": 0,
+                   "301": 0,
+                   "400": 0,
+                   "401": 0,
+                   "403": 0,
+                   "404": 0,
+                   "405": 0,
+                   "500": 0}
 
-try:
-    for line in sys.stdin:
-        signal
-        lineCount += 1
-        info = line.split()
-        totalSize += int(info[8])
-        if lineCount % 10 == 0:
-            print_info(totalSize, statusCodes)
-        if info[7] in statusCodes:
-            statusCodes[info[7]] = statusCodes.get(info[7]) + 1
-    print_info(totalSize, statusCodes)
+    try:
+        for line in sys.stdin:
+            lineCount += 1
+            info = line.split()
+            totalSize += int(info[8])
+            
+            if lineCount % 10 == 0:
+                print_info(totalSize, statusCodes)
+            if info[7] in statusCodes:
+                statusCodes[info[7]] = statusCodes.get(info[7]) + 1
 
-except KeyboardInterrupt:
+    except KeyboardInterrupt:
+        print_info(totalSize, statusCodes)
+        #    raise
+
     print_info(totalSize, statusCodes)
-    raise
