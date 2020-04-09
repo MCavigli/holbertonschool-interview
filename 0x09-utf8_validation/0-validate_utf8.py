6ca69650282415510e9bfd523404ddc0c4d5eba5
Marc <mcavigli@gmail.com>
2020-04-08 22:25:00 -0700

    Initial push

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def validUTF8(data):
+    ''' Determines if a given data set represents a valid UTF-8 encoding '''
+    if data:
+        return true
+    else:
+        return false
