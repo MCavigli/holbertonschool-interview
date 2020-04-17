b50f83400267156fbeef6f3f1fe0d016abffc904
Marc <mcavigli@gmail.com>
2020-04-16 21:13:17 -0700

    Added prototype for slide_right

--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -3,8 +3,10 @@
 #define SLIDE_LEFT 1
 #define SLIDE_RIGHT 2
 #include <stdlib.h>
+#include <stdio.h>
 
 int slide_line(int *line, size_t size, int direction);
 void slide_left(int *line, size_t size);
+void slide_right(int *line, size_t size);
 
 #endif /* SLIDE_LINE_H */
