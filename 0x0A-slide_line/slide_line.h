56d5784b7a22356cf70367080fef0fe611dae09f
Marc <mcavigli@gmail.com>
2020-04-17 15:24:56 -0700

    Moved around declarations

--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -1,10 +1,10 @@
 #ifndef SLIDE_LINE_H
 #define SLIDE_LINE_H
 
+#include <stdlib.h>
+
 #define SLIDE_LEFT 1
 #define SLIDE_RIGHT 2
-
-#include <stdlib.h>
 
 int slide_line(int *line, size_t size, int direction);
 
