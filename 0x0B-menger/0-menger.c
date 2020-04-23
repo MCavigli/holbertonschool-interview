f1d6a451d4e666d80e8baeaeb4ba3c2718da9ab7
Marc <mcavigli@gmail.com>
2020-04-23 11:36:43 -0700

    Fixed problem

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -9,12 +9,12 @@ void menger(int level)
 {
 	if (level < 0)
 		return;
-	int r = 0;
+	int r, i, j = 0;
 
 	r = pow(3, level);
-	for (int i = 0; i < r; i++)
+	for (; i < r; i++)
 	{
-		for (int j = 0; j < r; j++)
+		for (; j < r; j++)
 		{
 			putchar(35);
 		}
