3f207a4259135960948e093ceec5d46fc9b47f87
Marc <mcavigli@gmail.com>
2020-06-03 22:31:19 -0700

    Followed directions for order of names

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,29 +1,25 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-if (process.argv.length === 3) {
-  starwars(process.argv[2]);
-}
-
-async function starwars (argz) {
-  const url = 'https://swapi-api.hbtn.io/api/films/' + argz;
-  request(url, async function (err, res, body) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-
-    const characters = JSON.parse(body).characters;
-    for (let i = 0; i < characters.length; i++) {
-      request(characters[i], function (err, res, body) {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        const name = JSON.parse(body).name;
-        console.log(name);
-      });
-    }
-  });
-}
+const url = 'https://swapi.co/api/films/' + process.argv[2];
+request.get(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const characters = JSON.parse(body).characters;
+  const nameList = [];
+  for (let i = 0; i < characters.length; i++) {
+    request.get(characters[i], function (err, res, body) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      const name = JSON.parse(body).name;
+      nameList.push(name);
+    });
+  }
+  for (let j = 0; j < nameList.length; j++) {
+    console.log(nameList[j]);
+  }
+});
