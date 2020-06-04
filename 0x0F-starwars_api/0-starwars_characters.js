95f07c24f73eaca08e12d805d565e2315c3332d0
Marc <mcavigli@gmail.com>
2020-06-03 22:09:39 -0700

    Added async

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,22 +1,29 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (err, res, body) {
-  if (err) {
-    console.log(err);
-    return;
-  }
 
-  const characters = JSON.parse(body).characters;
-  for (let i = 0; i < characters.length; i++) {
-    request(characters[i], function (err, res, body) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      const name = JSON.parse(body).name;
-      console.log(name);
-    });
-  }
-});
+if (process.argv.length === 3) {
+  starwars(process.argv[2]);
+}
+
+async function starwars (argz) {
+  const url = 'https://swapi-api.hbtn.io/api/films/' + argz;
+  request(url, async function (err, res, body) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const characters = JSON.parse(body).characters;
+    for (let i = 0; i < characters.length; i++) {
+      request(characters[i], function (err, res, body) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        const name = JSON.parse(body).name;
+        console.log(name);
+      });
+    }
+  });
+}
