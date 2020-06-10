0257af615bf09c041e3d7f4e6a678d37971e51dc
Marc <mcavigli@gmail.com>
2020-06-09 23:50:54 -0700

    Semistandardized

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,27 +3,27 @@
 const request = require('request');
 
 if (process.argv.length === 3) {
-	sw(process.argv[2]);
+  sw(process.argv[2]);
 }
-async function sw(id) {
-	const url = `https://swapi-api.hbtn.io/api/films/` + id;
+async function sw (id) {
+  const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 
-	request(url, async function (err, response, body) {
-		if (err) {
-			console.log(err);
-		} else {
-			for (const chars of JSON.parse(body).characters) {
-				const promise = new Promise(function (resolve, reject) {
-					request(chars, function (err, response, body) {
-						if (err) {
-							console.log(err);
-						} else {
-							resolve(JSON.parse(body).name);
-						}
-					});
-				});
-				console.log(await promise);
-			};
-		}
-	});
+  request(url, async function (err, response, body) {
+    if (err) {
+      console.log(err);
+    } else {
+      for (const chars of JSON.parse(body).characters) {
+        const promise = new Promise(function (resolve, reject) {
+          request(chars, function (err, response, body) {
+            if (err) {
+              console.log(err);
+            } else {
+              resolve(JSON.parse(body).name);
+            }
+          });
+        });
+        console.log(await promise);
+      }
+    }
+  });
 }
