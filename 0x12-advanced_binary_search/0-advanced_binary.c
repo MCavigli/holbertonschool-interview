28e7e836e001f7920c7013076480e987e0e7b3e5
Marc <mcavigli@gmail.com>
2020-06-23 18:27:24 -0700

    Initial push

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,56 @@
+#include "search_algos.h"
+
+void print_array(int *array, int left, int right)
+{
+	int i = left;
+
+	printf("Searching in array:");
+	while (i <= right)
+	{
+		printf(" %i", array[i]);
+		i++;
+	}
+	printf("\n");
+}
+/**
+ * find_value - recursive call to find value position
+ * @array: pointer to new array position
+ * @right: size of the smaller array
+ * @value: value to search for
+ * @left: the index
+ */
+int find_value(int *array, int right, int value, int left)
+{
+	if (right >= left)
+	{
+		int mid = left + (right - left) / 2;
+
+		print_array(array, left, right);
+		if (array[mid] == value)
+			return (mid);
+		if (array[mid] > value)
+		{
+			return (find_value(array, mid - 1, value, 1));
+		}
+		else if (array[mid] < value)
+		{
+			return (find_value(array, right, value, mid + 1));
+		}
+	}
+	return (-1);
+}
+
+/**
+ * advanced_binary - searches for a value in a sorted array of integers
+ * @array: pointer to first element in an array
+ * @size: the size of the array
+ * @value: the value to find
+ * Return: the index where the value is, -1 if failure
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	int result = find_value(array, size - 1, value, 0);
+
+	return (result);
+}
---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+#include <stdio.h>
+#include <stdlib.h>
+
+int advanced_binary(int *array, size_t size, int value);
+int find_value(int *array, int right, int value, int left);
+
+#endif /* SEARCH_ALGOS_H */
