a1b3f5077755e4add1292d9801b349634ce311f9
Marc Cavigli <mcavigli@gmail.com>
2020-07-16 05:00:54 +0000

    Refactored code

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-/** main - Entry point. Multiplies two numbers
+/**
+ * main - Entry point. Multiplies two numbers
  * @argc: number of args
  * @argv: argument vector
  * Return: The product or 98 on failure
@@ -11,8 +12,7 @@
 
 int main(int argc, char *argv[])
 {
-	int argLen1, argLen2;
-	int c, n1, n2, sum, i, j;
+	int argLen1, argLen2, c, n1, n2, i, j, i_n1, i_n2;
 	int i_n1, i_n2;
 	int *product;
 
@@ -21,42 +21,35 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	argLen1 = strlen(argv[1]);
-	argLen2 = strlen(argv[2]);
-
-	product = calloc(argLen1 + argLen2, sizeof(*product));
-
-	i_n1 = 0;
-	i_n2 = 0;
+	argLen1 = strlen(argv[1]), argLen2 = strlen(argv[2]);
+	product = malloc((argLen1 + argLen2) * sizeof(*product));
+	if (!product)
+		exit(98);
+	i_n1 = 0, i_n2 = 0;
 	for (i = argLen1 - 1; i >= 0; i--)
 	{
 		c = 0;
 		n1 = argv[1][i] - '0';
 		i_n2 = 0;
-
 		for (j = argLen2 - 1; j >= 0; j--)
 		{
 			n2 = argv[2][j] - '0';
-			sum = ((n1 * n2) + (product[i + j])) + c;
-			product[i + j] = sum % 10;
-			c = sum / 10;
+			c += (n1 * n2) + product[i_n1 + i_n2];
+			product[i_n1 + i_n2] = c % 10;
+			c = c / 10;
 			i_n2++;
 		}
 		if (c > 0)
 			product[i_n1 + i_n2] += c;
 		i_n1++;
 	}
-	if (product[0] == 0)
-		i = 1;
-	else
-		i = 0;
-
-	while (i < argLen1 + argLen2)
+	i = strlen((char *)product);
+	while (i >= 0)
 	{
 		printf("%d", product[i]);
-		i++;
+		i--;
 	}
 	printf("\n");
 	free(product);
-	return (EXIT_SUCCESS);
+	return (1);
 }
