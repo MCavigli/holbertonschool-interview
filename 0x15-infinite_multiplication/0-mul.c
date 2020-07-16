f5351239ffad9cbe17361a376a5de2e8362921a3
Marc Cavigli <mcavigli@gmail.com>
2020-07-16 02:45:40 +0000

    Rewrote code

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -1,5 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /** main - Entry point. Multiplies two numbers
  * @argc: number of args
@@ -9,18 +11,48 @@
 
 int main(int argc, char *argv[])
 {
-	char *one, *two;
-	int product;
+	int argLen1, argLen2;
+	int c, n1, n2, sum, i, j;
+	int i_n1, i_n2;
+	int *product;
 
 	if (argc != 3)
-		return (98);
+	{
+		printf("Error\n");
+		exit (98);
+	}
+        argLen1 = strlen(argv[1]);
+        argLen2 = strlen(argv[2]);
 
-	one = argv[1];
-	two = argv[2];
-	product = (int)one * (int)two;
+	product = calloc(argLen1 + argLen2, sizeof(*product));
 
-	printf("One: %s\n", one);
-	printf("Two: %s\n", two);
-	printf("Product: %i\n", product);
-	return (1);
+	i_n1 = 0;
+	i_n2 = 0;
+	for (i = argLen1 - 1; i >= 0; i--)
+	{
+		c = 0;
+		n1 = argv[1][i] - '0';
+		i_n2 = 0;
+
+		for (j = argLen2 - 1; j >= 0; j--)
+		{
+			n2 = argv[2][j] - '0';
+			sum = n1 * n2 + product[i + j] + c;
+			product[i + j] = sum % 10;
+			c = sum/10;
+			i_n2++;
+		}
+		if (c > 0)
+			product[i_n1 + i_n2] += c;
+		i_n1++;
+	}
+	i = product[0] == 0 ? 1 : 0;
+	while (i < argLen1 + argLen2)
+	{
+		printf("%d", product[i]);
+		i++;
+	}
+	printf("\n");
+	free(product);
+	return (EXIT_SUCCESS);
 }
