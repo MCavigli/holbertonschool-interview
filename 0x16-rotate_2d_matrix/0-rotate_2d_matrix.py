b48330b6c4e180018ddb23125b3d766621b2bb6c
Marc <mcavigli@gmail.com>
2020-07-20 14:34:46 -0700

    Rewrote code to rotate by a specific amount

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,4 +5,12 @@
 def rotate_2d_matrix(matrix):
     ''' Rotates a 2D matrix '''
 
-    matrix[:] = matrix[::-1]
+    N = len(matrix)
+    for _ in range(3):
+        for x in range(0, int(N / 2)):
+            for y in range(x, N - x - 1):
+                temp = matrix[x][y]
+                matrix[x][y] = matrix[y][N - 1 - x]
+                matrix[y][N - 1 - x] = matrix[N - 1 - x][N - 1 - y]
+                matrix[N - 1 - x][N - 1 - y] = matrix[N - 1 - y][x]
+                matrix[N - 1 - y][x] = temp
