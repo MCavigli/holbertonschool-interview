ee3e6b270b7ae86dbf103a553dfd7c93f6051fa0
Marc <mcavigli@gmail.com>
2020-08-25 14:24:21 -0700

    Initial push

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Method that finds the fewest number of coins to use for
+a given value
+"""
+
+
+def makeChange(coins, total):
+    if total <= 0:
+        return (0)
+    newTotal = total
+    count = 0
+    coins.sort(reverse=True)
+    for num in coins:
+        currentNum = num
+        print("CurrentNum: ", currentNum)
+        while (currentNum <= newTotal):
+            print("newTotal at beginning of while: ", newTotal)
+            print("count at beginning of while: ", count)
+            newTotal = newTotal - num
+            count += 1
+            print("newTotal at end of while: ", newTotal)
+            print("count at end of while: ", count)
+        print("***Exited while loop***")
+    return (count)
