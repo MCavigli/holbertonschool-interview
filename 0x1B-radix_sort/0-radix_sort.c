62d542fe57fcd7fa57dcb1d518d25cc9f9e29ec4
Marc <mcavigli@gmail.com>
2020-09-08 21:30:00 -0700

    Initial commit

---
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -0,0 +1,62 @@
+#include "sort.h"
+
+/**
+ * radix_sort - sorts an array using radix LSD method
+ * @array: the array to sort
+ * @size: size of the array
+ */
+void radix_sort(int *array, size_t size)
+{
+	int exp;
+	int max = getMax(array, size);
+
+	for (exp = 1; max / exp > 0; exp *= 10)
+	{
+		countSort(array, size, exp);
+		print_array(array, size);
+	}
+}
+
+/**
+ * getMax - gets the max number in an array
+ * @array: array to search
+ * @size: size of the array
+ * Return: the biggest number in the array
+ */
+int getMax(int *array, size_t size)
+{
+	size_t i;
+	int max = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[0];
+	}
+	return (max);
+}
+
+/**
+ * countSort - counting sort of the array
+ * @array: the array to sort
+ * @size: size of the array
+ * @exp: digit to count around
+ */
+void countSort(int *array, size_t size, int exp)
+{
+	int output[size];
+	size_t i;
+	int count[10] = {0};
+
+	for (i = 0; i < size; i++)
+		count[(array[i] / exp) % 10]++;
+	for (i = 1; i < 10; i++)
+		count[i] += count[i - 1];
+	for (i = size - 1; i >= 0; i--)
+	{
+		output[count[(array[i] / exp) % 10] - 1] = array[i];
+		count[(array[i] / exp) % 10]--;
+	}
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
+}
---
+++ 0x1B-radix_sort/sort.h
@@ -0,0 +1,10 @@
+#ifndef SORT_H
+#define SORT_H
+#include <stdio.h>
+
+void print_array(const int *array, size_t size);
+void radix_sort(int *array, size_t size);
+int getMax(int *array, size_t size);
+void countSort(int *array, size_t size, int exp);
+
+#endif /* SORT_H */
