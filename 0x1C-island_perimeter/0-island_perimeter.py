7525592b909792dfd5dea11f2c2d0f71fd9b0938
Marc <mcavigli@gmail.com>
2020-09-14 15:04:14 -0700

    Changed conditional

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -8,13 +8,13 @@ def island_perimeter(grid):
     """ Returns the perimiter of @grid"""
     pm = 0
     for x in range(len(grid)):
-        for y in range(len(grid[x])):
-            if grid[x][y] == 1:
-                if x > 0 and grid[x - 1][y] == 0:
+        for y in range(len(grid[0])):
+            if grid[x][y]:
+                if x and grid[x - 1][y] == 0:
                     pm += 1
                 if x < len(grid) and grid[x + 1][y] == 0:
                     pm += 1
-                if y > 0 and grid[x][y - 1] == 0:
+                if y and grid[x][y - 1] == 0:
                     pm += 1
                 if y < len(grid[x]) and grid[x][y + 1] == 0:
                     pm += 1
