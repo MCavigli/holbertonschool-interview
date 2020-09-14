9901d06f17b1ce605d76001c56e549ad24b0a0e8
Marc <mcavigli@gmail.com>
2020-09-14 13:50:44 -0700

    Initial push

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Finds the perimiter of an island.
+"""
+
+def island_perimeter(grid):
+    pm = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] == 1:
+                if x > 0 and grid[x - 1][y] == 0:
+                    pm += 1
+                if x < len(grid) and grid[x + 1][y] == 0:
+                    pm += 1
+                if y > 0 and grid[x][y - 1] == 0:
+                    pm += 1
+                if y < len(grid[x]) and grid[x][y + 1] == 0:
+                    pm += 1
+    return (pm)
