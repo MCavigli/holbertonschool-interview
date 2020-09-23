9d4c4fdf12abb986def0b0dba863341ccd435a6d
Marc <mcavigli@gmail.com>
2020-09-23 13:12:44 -0700

    Changed logic to subtract from total possible numbers of surrounding sides instead of adding. This will account for land on the edge of the square.

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -10,12 +10,9 @@ def island_perimeter(grid):
     for x in range(len(grid)):
         for y in range(len(grid[0])):
             if grid[x][y]:
-                if x and grid[x - 1][y] == 0:
-                    pm += 1
-                if x < len(grid) and grid[x + 1][y] == 0:
-                    pm += 1
-                if y and grid[x][y - 1] == 0:
-                    pm += 1
-                if y < len(grid[x]) and grid[x][y + 1] == 0:
-                    pm += 1
+                pm += 4
+                if x and grid[x - 1][y]:
+                    pm -= 2
+                if y and grid[x][y - 1]:
+                    pm -= 2
     return (pm)
