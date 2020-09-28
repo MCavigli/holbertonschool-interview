5e98f4723d54e105de4e84aaed9eb643d20f02a5
Marc <mcavigli@gmail.com>
2020-09-28 14:22:22 -0700

    Initial commit

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - compares two strings
+ * @s1: First string
+ * @s2: Second string
+ * Return: 1 if the strings can be considered identical, 0 if not
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+}
