ef63d0a36887aa2a3db024b9e222ba4e7abde7da
Marc <mcavigli@gmail.com>
2020-10-16 12:11:39 -0700

    Added actual code

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -10,3 +10,11 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return ([])
+    lst = [[] for i in range(0, n)]
+    for i in range(0, n):
+        for j in range(i + 1):
+            if (j < i):
+                lst[i].append(lst[i - 1][j] + lst[i - 1][j - 1])
+            elif (i == j):
+                lst[i].append(1)
+    return (lst)
