152bb5aeecdce085d666138c0ac5c152a396646f
Marc <mcavigli@gmail.com>
2020-10-16 12:05:08 -0700

    Initial push

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+0-pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of integers representing Pascal's triangle
+    """
+    if n <= 0:
+        return ([])
