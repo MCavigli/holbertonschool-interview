c5174e3873e7e0a8ff06efea79c66d71b49e2e10
Marc <mcavigli@gmail.com>
2020-10-16 12:15:28 -0700

    Added conditional if index was equal to or less than 0

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -8,13 +8,14 @@ def pascal_triangle(n):
     """
     Returns a list of integers representing Pascal's triangle
     """
-    if n <= 0:
-        return ([])
     lst = [[] for i in range(0, n)]
     for i in range(0, n):
         for j in range(i + 1):
             if (j < i):
-                lst[i].append(lst[i - 1][j] + lst[i - 1][j - 1])
+                if (i <= 0):
+                    lst[i].append(1)
+                else:
+                    lst[i].append(lst[i - 1][j] + lst[i - 1][j - 1])
             elif (i == j):
                 lst[i].append(1)
     return (lst)
