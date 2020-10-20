1db0ef7543f63e8b748163dec5bf4d61bb0a7c11
Marc <mcavigli@gmail.com>
2020-10-20 13:58:46 -0700

    Added functionality code

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -10,8 +10,18 @@ int regex_match(char const *str, char const *pattern)
 {
 	if (!pattern || !str)
 		return (0);
-	if (!*pattern)
-		return (!*str);
 
+	if (*(pattern + 1) == '*')
+	{
+		if (regex_match(str, pattern + 2))
+			return (1);
+		while (*str && (*pattern == *(str++)))
+		{
+			if (regex_match(str, pattern + 2))
+				return (1);
+		}
+	}
+	else if (*pattern == *str || (*pattern == '.' && *str))
+		return (regex_match(++str, ++pattern));
 	return (0);
 }
